dbab420e0d5c99e76de81d644622845ce8deac89
Hugo Alliaume <kocal@live.fr>
2018-03-10 21:50:55 +0100

    i18n(fr): vuetify/Home.js

--- lang/en/vuetify/Home.js
+++ lang/en/vuetify/Home.js
@@ -1,56 +1,56 @@
 export default {
-  announcementBanner: 'Aww yisss, this is an exciting announcement! Or maybe its an advertisement!',
+  announcementBanner: "Oh yeah, c'est une annonce importante ! Ou alors c'est une publicité !",
   heading1: 'Vuetify',
   heading1cont: 'Material Component Framework',
-  getStarted: 'Get Started',
+  getStarted: 'Commencer',
   features: [
     {
       img: '/static/doc-images/feature1.svg',
-      title: 'Ready-Made Project Scaffolding',
-      text: 'Vuetify comes ready to go with 8 pre-made vue-cli templates. From simple html to full-blown SSR you are ready to go in minutes.'
+      title: "Des fondations de projets prêtes à l'emploi",
+      text: 'Vuetify propose 8 templates vue-cli. Du simple HTML au véritable SSR, vous êtes prêts à développer en quelques minutes.'
     },
     {
       img: '/static/doc-images/feature2.svg',
-      title: 'Semantic Material Components',
-      text: 'Be prepared for an armada of specialized components at your disposal. With over 80 in total, there is a solution for any application.'
+      title: 'Composants Material et sémantiques',
+      text: 'Préparez-vous à une armada de composants spécialiés mis à votre disposition. Avec plus de 80 composants, il y a une solution pour chaque application.'
     },
     {
       img: '/static/doc-images/feature3.svg',
-      title: 'Vibrant Community',
-      text: 'When you run into a roadblock, you need assistance right away. Vuetify offers chat support in our growing community on Discord.'
+      title: 'Communauté dynamique',
+      text: "Lorsque vous rencontrez un obstacle, vous avez immédiatement besoin d'aide. Vuetify propose un support en ligne par chat grâce à la communauté grandissante de notre Discord."
     }
   ],
-  checkFeaturesTitle: 'All the Tools You Need',
-  checkFeaturesTitleCtd: 'To Build Incredible User Interfaces',
+  checkFeaturesTitle: 'Tous les outils dont vous avez besoin',
+  checkFeaturesTitleCtd: "Pour créer d'incroyables interfaces utilisateurs",
   checkFeatures: [
-    'Community Chat Support',
-    'Dynamic Themes',
-    'SSR and PWA Support',
-    '8 vue-cli Templates'
+    'Soutien communautaire',
+    'Thèmes dynamiques',
+    'Support du SSR and PWA',
+    '8 templates vue-cli'
   ],
   checkFeaturesCtd: [
-    '80+ Material Design Components',
-    'A La Carte Importing',
-    'Transition Components',
-    'Blazing Fast'
+    'Plus de 80+ composants Material Design',
+    'Imports A La Carte',
+    'Composants de transition',
+    'Ultra-rapide'
   ],
   componentFramework: 'Component Framework',
   letterFromAuthor: [
-    'Hey there,',
-    'I know choosing a UI framework can be a daunting task -- there are so many options to choose from and every one of them claims that they are the best. So you have to decide for yourself: what are the metrics for success?',
-    'This was me almost a year ago. There were plenty of UI toolkits to choose from, but none of them had everything that I needed, and that is why I started working on Vuetify.',
-    'I am extremely humbled that you have considered using Vuetify as your next UI Framework. A lot of care has went into crafting the perfect setup for taking any idea from conception to production. I look forward to you joining the Vuetify community and am excited to see what incredible interfaces you create!'
+    'Hey',
+    "Je sais que choisir un framework front-end peut être une tâche décourageante -- il y a tellement d'options à prendre en compte et chacun d'entre eux se proclame d'être le meilleur. Donc vous devez décider-vous même, quels sont les paramètres du succès ?",
+    "C'était moi il y a un an. Il fallait choisir parmi des tas d'outils front-end, mais aucun d'entre eux avait tout ce dont j'avais besoin, et c'est pour cela que j'ai commencé à travailler sur Vuetify.",
+    "Je suis extrêmement touché que vous avez considéré à utiliser Vuetify en tant que votre prochain framework front-end. Beaucoup d'attention a été consacrée dans l'élaboration d'une configuration parfaite pour développer n'importe quelle idée issue de la conception jusqu'à la production. J'attend avec impatience votre participation à la communauté Vuetify, et je suis excité de voir quelles choses incroyables vous allez créer !"
   ],
-  authorOfVuetify: 'Author of Vuetify',
+  authorOfVuetify: 'Auteur de Vuetify',
   support: {
-    hasVuetifyHelped: 'Has Vuetify helped you create an amazing application?',
-    showYourSupport: 'Show your support for Vuetify by',
-    becomingAPatron: 'becoming a patron'
+    hasVuetifyHelped: 'Est-ce que Vuetify vous a aidé à créer de superbes applications ?',
+    showYourSupport: 'Apportez votre soutien à Vuetify en',
+    becomingAPatron: 'devenant un patron'
   },
-  becomeSponsor: 'Become a Sponsor',
-  proudlySponsoredBy: 'Patreon Sponsors',
-  callout: 'Supercharge your Development',
-  madeWithVuetify: 'Projects Made With Vuetify',
+  becomeSponsor: 'Devenir un Sponsor',
+  proudlySponsoredBy: 'Sponsors Patreon',
+  callout: 'Boostez votre productivité',
+  madeWithVuetify: 'Projets créés avec Vuetify',
   materialDesign: 'Material Design',
-  mit: 'MIT License'
+  mit: 'Licence MIT'
 }
