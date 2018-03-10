423346afea0de3753b6be31d7deb3b4fe1879f07
Hugo Alliaume <kocal@live.fr>
2018-03-10 16:23:04 +0100

    i18n(fr): rename Github to GitHub

--- lang/en/vuetify/AppToolbar.js
+++ lang/en/vuetify/AppToolbar.js
@@ -42,7 +42,7 @@ export default {
     },
     {
       href: 'https://github.com/vuetifyjs/vuetify/issues',
-      text: 'Voir les tickets Github',
+      text: 'Voir les tickets GitHub',
       icon: 'mdi-github-face'
     },
     {
@@ -56,7 +56,7 @@ export default {
   socials: [
     {
       href: 'https://github.com/vuetifyjs/vuetify',
-      text: 'Github',
+      text: 'GitHub',
       icon: 'mdi-github-box'
     },
     {
